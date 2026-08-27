1abc12e4c93e73d0c4487d4fa12c117281abf4fc
DonatasNoreika <donoras@gmail.com>
2026-08-27 18:58:01 +0300

    +Prisijungęs vartotojas gali pakomentuoti postą

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,5 +1,11 @@
-from .models import CustomUser
+from .models import CustomUser, Comment
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
 
 class CustomUserCreateForm(UserCreationForm):
     class Meta:
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -54,3 +54,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.content} ({self.author})"
+
+    class Meta:
+        ordering = ['-pk']
