cdca368e226163fadb75ca648cba343820281dec
DonatasNoreika <donoras@gmail.com>
2026-08-27 19:35:04 +0300

    +Prisijungęs vartotojas gali redaguoti savo komentarus

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -128,3 +128,14 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView
     def test_func(self):
         return self.get_object().author == self.request.user
 
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = "form.html"
+
+    def get_success_url(self):
+        return reverse("post", kwargs={"pk": self.object.post.pk})
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
